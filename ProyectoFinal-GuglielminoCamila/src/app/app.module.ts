511d51abdb14ae2009a68a7e5b554b79d39b4c96
Camila Guglielmino <camila.guglielmino@gmail.com>
2022-07-30 19:22:10 -0300

    se agrego bootstrap

--- ProyectoFinal-GuglielminoCamila/src/app/app.module.ts
+++ ProyectoFinal-GuglielminoCamila/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgComponent
+    LogoArgComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
